144f7922e4a1c02347629136640c3c3d41a5473a
frank <ohcool9898@gmail.com>
2015-04-28 11:49:26 +0800

    1.Add some colors into Color.h.

--- Color.h
+++ Color.h
@@ -4,7 +4,7 @@ class PeerList;
 
 struct Color
 {
-	enum Value{white = 0,nat,red,purple,blue,brown,orange,violet,cyan,seagreen};
+	enum Value{white = 0,nat,red,purple,blue,brown,orange,violet,cyan,seagreen,lawngreen,springgreen,cornflowerblue,magenta,darkgoldenrod,chololate,gold,tomato,deeppink,orangered,darkolivegreen,indianred,lightpink};
 	const static char * toString(int value)
 	{
 		switch (value)
@@ -29,6 +29,32 @@ struct Color
 				return "violet";
 			case cyan:
 				return "cyan";
+			case lawngreen:
+				return "lawgreen";
+			case springgreen:
+				return "springgreen";
+			case cornflowerblue: 
+				return "cornflowerblue"; 
+			case magenta:
+				return "magenta";
+			case darkgoldenrod: 
+				return "darkgoldenrod";
+			case chololate:
+				return  "chololate";
+			case  gold:
+				return "gold";
+			case tomato:
+				return "tomato";
+			case deeppink:
+				return "deeppink";
+			case orangered:
+				return "orangered";
+			case darkolivegreen:
+				return  "darkolivegreen";
+			case indianred:
+				return "indianred";
+			case lightpink:
+				return "lightpink";
 			default:
 				return "white";
 		}
